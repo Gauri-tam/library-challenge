a94d1faf68751e5a931156bdb5e578691a8bb634
gauri <gauri@weetechsolution.com>
2024-02-10 18:11:54 +0530

    ctraing the authentication Entry point To Create the response message

---
+++ src/main/java/com/jwtAuthLibrary/jwtBookAuthor/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.jwtAuthLibrary.jwtBookAuthor.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException, ServletException {
+
+       response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("Access Fail ! " +authException.getMessage());
+    }
+}
