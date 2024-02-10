4a9aea700c20f3ca005556b37949b11246bf5c82
gauri <gauri@weetechsolution.com>
2024-02-10 18:13:46 +0530

    creating the logout service to logout the user and also expired that user token

---
+++ src/main/java/com/jwtAuthLibrary/jwtBookAuthor/service/LogOutService.java
@@ -0,0 +1,32 @@
+package com.jwtAuthLibrary.jwtBookAuthor.service;
+
+import com.jwtAuthLibrary.jwtBookAuthor.repository.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogOutService implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String token;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            return;
+        }
+        token = authHeader.substring(7);
+        var storeUser = tokenRepository.findUserByToken(token).orElseThrow();
+       if (storeUser != null){
+           storeUser.setRevoked(true);
+           storeUser.setRevoked(true);
+           tokenRepository.save(storeUser);
+       }
+    }
+}
